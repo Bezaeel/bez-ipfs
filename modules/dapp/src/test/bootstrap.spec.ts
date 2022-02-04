0bb8f3d41baa703a3f99bede9fa75067d4a13a53
Bezaeel <talabi.rotimi@hotmail.com>
2022-02-04 16:40:12 +0100

    add resource to enable test

--- modules/dapp/src/test/bootstrap.spec.ts
+++ modules/dapp/src/test/bootstrap.spec.ts
@@ -5,7 +5,7 @@ import { Bootstrap } from "../bootstrap";
 describe("Storage", async function () {
     it("Should be able to store correctly", async function () {
       const bootstrap = new Bootstrap();
-      const file = bootstrap.uploadFile("/home/talabi/Pictures/error.png");
+      const file = bootstrap.uploadFile(__dirname + "/resource/error.png");
 
       assert.isNotNull(file);
     });
